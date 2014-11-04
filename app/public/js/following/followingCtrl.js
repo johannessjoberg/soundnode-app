18d0e57c09224578b5542dabc3fb11df2add17e1
Johannes Sjöberg <johannes.sjoberg@live.se>
2014-11-04 15:05:24 +0100

    Started on the following functionality. Current state listing followed user, with picture, and number of tracks

--- app/public/js/following/followingCtrl.js
+++ app/public/js/following/followingCtrl.js
@@ -1,5 +1,40 @@
 'use strict';
 
-app.controller('FollowingCtrl', function ($scope, $http) {
-    $scope.title = 'Following view';
+app.controller('FollowingCtrl', function ($scope, SCapiService, $rootScope) {
+    $scope.title = 'Following view:';
+    $scope.data = '';
+    $scope.busy = false;
+
+    var endpoint = 'me/followings'
+        , params = '';
+
+    SCapiService.get(endpoint, params)
+        .then(function(data) {
+            $scope.data = data;
+            console.log("data:", data);
+        }, function(error) {
+            console.log('error', error);
+        }).finally(function() {
+            $rootScope.isLoading = false;
+        });
+
+    $scope.loadMore = function() {
+        if ( $scope.busy ) {
+            return;
+        }
+        $scope.busy = true;
+
+        SCapiService.getNextPage()
+            .then(function(data) {
+                for ( var i = 0; i < data.collection.length; i++ ) {
+                    $scope.data.push( data.collection[i] )
+                }
+                $scope.busy = false;
+            }, function(error) {
+                console.log('error', error);
+            }).finally(function(){
+                $rootScope.isLoading = false;
+            });
+    };
+
 });
