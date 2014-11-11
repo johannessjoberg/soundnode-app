ec0900c5da99b0fc589f6fcd5de6da846241a603
joellundell <joel.lundell@gmail.com>
2014-11-11 15:47:00 +0100

    Added next page functionality for searching

--- app/public/js/search/searchCtrl.js
+++ app/public/js/search/searchCtrl.js
@@ -6,14 +6,35 @@ app.controller('searchCtrl', function ($scope, $http, $stateParams, SCapiService
 
     $scope.title = 'Results for: ' + $stateParams.q;
     $scope.data = '';
+    var next_url = '';
 
     SCapiService.get(endpoint, params)
         .then(function(data) {
-            $scope.data = data;
+            $scope.data = data.collection;
+            next_url = data.next_href;
         }, function(error) {
             console.log('error', error);
         }).finally(function(){
             $rootScope.isLoading = false;
         });
+
+    $scope.loadMore = function() {
+        if ($scope.busy) {
+            return;
+        }
+        $scope.busy = true;
+         SCapiService.getNextPage(next_url)
+             .then(function(data) {
+                 for ( var i = 0; i < data.collection.length; i++ ) {
+                     $scope.data.push( data.collection[i] )
+                 }
+                 next_url = data.next_href;
+             }, function(error) {
+                 console.log('error', error);
+             }).finally(function() {
+                 $scope.busy = false;
+                 $rootScope.isLoading = false;
+             });
+    };
 
 });
