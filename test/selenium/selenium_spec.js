36e931c6af3eba251224e95fa9859bc8117a6bf8
Jon Kristensen <info@jonkri.com>
2014-12-18 09:33:56 +0100

    selenium_spec.js: Replace the example test

--- test/selenium/selenium_spec.js
+++ test/selenium/selenium_spec.js
@@ -1,20 +1,32 @@
 'use strict';
 
-var assert = require('assert');
+var assert = require('assert'),
+    DELAY = 5000,
+    assertNoError = function(e, r) { assert(e == undefined); };
 
 describe('Selenium suite', function(done) {
-    it("has a working expand button", function(done) {
+    it("has working show/hide buttons for playlists", function(done) {
         browser
-            .pause(5000)
-            // Click on the maximize button.
-            .click('#expandApp', function(error, result) {
-                // TODO: Assess that the section is expanded.
-
-                // console.log('Clicked on the maximize button...');
-                // console.log('error: ' + error);
-                // console.log('result: ' + result);
-
-                // expect(sectionExpanded).toBe(true);
+            .pause(DELAY)
+            .click('a[ui-sref="playlists"] span', assertNoError) // Playlists
+            .pause(DELAY)
+            .getCssProperty('li.songList_item', 'height', function(e, r) {
+                assert(r[0].value === '15px');
+            })
+            .click('h4.songList_item_song_user span', assertNoError) // Show
+            .pause(DELAY)
+            .getCssProperty('li.songList_item', 'height', function(e, r) {
+                assert(parseInt(r[0].value) > 15);
+            })
+            .click('h4.songList_item_song_user span', assertNoError) // Hide
+            .pause(DELAY)
+            .getCssProperty('li.songList_item', 'height', function(e, r) {
+                assert(r[0].value === '15px');
+            })
+            .click('h4.songList_item_song_user span', assertNoError) // Show
+            .pause(DELAY)
+            .getCssProperty('li.songList_item', 'height', function(e, r) {
+                assert(parseInt(r[0].value) > 15);
             })
             .call(done);
     });
